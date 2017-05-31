05e5c1c7e30d399d7a84da2738434bb33f5f7483
pjb4752 <pjb4752@gmail.com>
2017-05-31 18:41:31 -0400

    clean up database initialization

--- src/main/scala/clunk/jdbc/Database.scala
+++ src/main/scala/clunk/jdbc/Database.scala
@@ -3,7 +3,7 @@ package clunk.jdbc
 import java.sql.DriverManager
 
 object Database {
-  val url = "jdbc:mysql://localhost/clunk"
+  val base = "jdbc:mysql://localhost/clunk"
   val user = "root"
   val password = ""
   val options = Map(
@@ -11,15 +11,13 @@ object Database {
     ("characterEncoding", "UTF-8"),
     ("serverTimezone", "UTC"))
 
-  def init(): Unit = {
-    Class.forName("com.mysql.cj.jdbc.Driver")
-  }
+  val _ = Class.forName("com.mysql.cj.jdbc.Driver")
 
   def connection() = {
-    val underlying = DriverManager.getConnection(uri, user, password)
+    val underlying = DriverManager.getConnection(url, user, password)
     new Connection(underlying)
   }
 
-  lazy val uri = url + options.map({ case (k, v) => s"${k}=${v}" }).
+  lazy val url = base + options.map({ case (k, v) => s"${k}=${v}" }).
     mkString("?", "&", "").toString
 }
